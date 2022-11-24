2de578ac4e0da32ba3bcd5bab326ee855fe4c7a7
Vinni949 <83116455+Vinni949@users.noreply.github.com>
2022-11-24 14:42:40 +0300

    AddDBClass

---
+++ PromoCode/Models/DBPromoCode.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PromoCode.Models
+{
+    public class DBPromoCode : DbContext
+    {
+
+        public DBPromoCode()
+        {
+        }
+
+        public DBPromoCode(DbContextOptions<DBPromoCode> options) : base(options)
+        {
+
+        }
+        public DbSet<PromoCode> PromoCode { get; set; }
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            List<string> s = new List<string>();
+            StreamReader f = new StreamReader("test.txt");
+            while (!f.EndOfStream)
+            {
+                s.Add(f.ReadLine());
+            }
+            optionsBuilder.UseSqlServer(s[2]);
+        }
+
+
+    }
+}
+
+
