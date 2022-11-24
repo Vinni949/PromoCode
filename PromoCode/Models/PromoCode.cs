3422333f97d528e2a7c035795e3245c420b3d98e
Vinni949 <83116455+Vinni949@users.noreply.github.com>
2022-11-24 14:38:16 +0300

    Добавьте файлы проекта.

---
+++ PromoCode/Models/PromoCode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PromoCode.Models
+{
+    public class PromoCode
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime dataTimeCreated { get; set; }
+        public DateTime activationDate { get; set; }
+        public Boolean activaton { get; set; }
+    }
+}
